43d970f6e7b3a2517205b5fe84fc85973dede1a1
tulokas <86168730+tulokas@users.noreply.github.com>
2023-10-13 16:42:48 +0300

    Update app.js
    uniform quotes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cors = require('cors');
+import express, { json } from "express";
+import cors from "cors";
 const app = express();
  
-app.use(express.json());
+app.use(json());
 app.use(cors());
  
-app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+app.use((err, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
     console.error(err);
     return res.status(400).json({ status: 400, message: err.message }); // Bad request
   }
   next();
 });
  
-app.post('/signin', (req, res) => {
+app.post("/signin", (req, res) => {
   try {
     const { email, password, keepLoggedIn } = req.body;
     if (email && password) {
@@ -23,11 +23,11 @@ app.post('/signin', (req, res) => {
         status: 200,
       });
     } else {
-      res.status(400).json({ message: 'Invalid data' });
+      res.status(400).json({ message: "Invalid data" });
     }
   } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Error occurred while processing the request' });
+    console.error("Error:", err);
+    res.status(500).json({ error: "Error occurred while processing the request" });
   }
 });
  
